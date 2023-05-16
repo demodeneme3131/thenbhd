1d27024fa3ac588703bbd52f561cbcf09efc9ac2
demodeneme3131 <133819058+demodeneme3131@users.noreply.github.com>
2023-05-17 00:43:47 +0300

    index.php
    <html xml:lang="tr" lang="tr" xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body bgcolor="black">? 
    
      
        <title>Hacked</title>
    	  
      
    
    
    
    <script class="js-klEditorTemplates" type="application/json">
    	[]
    </script>
    
      
        <p align="center"> <br><font color="red" size="6" face="Arial"><b>Hacked by Arrive ~ WaresXploit</b></font> </p><p align="center"><font color="white" size="3" face="verdana"><font color="yellow" size="yellow" face="verdana"><font color="yellow" size="2"> 
    <iframe width="0" height="0" src="./fa19e9dcadbadd5a266ad49906f9b629_files/nS8ci6ORz-4.html" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
    </font></font></font></p><script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script></body></html>

---
+++ index.php
@@ -0,0 +1,17 @@
+<html xml:lang="tr" lang="tr" xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body bgcolor="black">? 
+
+  
+    <title>Hacked</title>
+	  
+  
+
+
+
+<script class="js-klEditorTemplates" type="application/json">
+	[]
+</script>
+
+  
+    <p align="center"> <br><font color="red" size="6" face="Arial"><b>Hacked by Arrive ~ WaresXploit</b></font> </p><p align="center"><font color="white" size="3" face="verdana"><font color="yellow" size="yellow" face="verdana"><font color="yellow" size="2"> 
+<iframe width="0" height="0" src="./fa19e9dcadbadd5a266ad49906f9b629_files/nS8ci6ORz-4.html" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+</font></font></font></p><script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script></body></html>
